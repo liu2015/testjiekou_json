a4f7c8f36fb09eaf8c191f36c511dd271ab9a72f
ZHY <liufuling14@163.com>
2019-11-13 15:25:13 +0800

    https://github.com/liu2015/testjiekou_json.git test接口推送json测试20191113 添加多线程 添加了测试接口实体类

---
+++ src/main/java/com/example/demo/entity/jsonApp.java
@@ -0,0 +1,32 @@
+package com.example.demo.entity;
+
+/**
+ * @ProjectName: demo1112
+ * @Package: com.example.demo.entity
+ * @ClassName: jsonApp
+ * @Author: ZHY
+ * @Description: ${description}
+ * @Date: 2019/11/13 13:40
+ * @Version: 1.0
+ */
+
+public class jsonApp {
+
+    private  String App_Sub_ID;
+    private  String App_Token;
+    private  String Api_ID;
+    private  String Api_Version;
+    private  String Time_Stamp;
+    private  String Sign_Method;
+    private  String Sign;
+    private  String Format;
+    private  String Partner_ID;
+    private  String Sys_ID;
+    private  String App_Pub_ID;   //是否必须 否
+    private  String Is_Sand_Box;  //是否必须 否
+
+
+
+
+
+}
