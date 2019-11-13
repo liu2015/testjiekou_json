e170bd7d06c42876aaf0c53953831bbf1847beda
ZHY <liufuling14@163.com>
2019-11-13 10:58:22 +0800

    https://github.com/liu2015/testjiekou_json.git test接口推送json测试20191113 添加多线程

--- src/main/java/com/example/demo/task/Scheduleconfig.java
+++ src/main/java/com/example/demo/task/Scheduleconfig.java
@@ -1,11 +1,14 @@
 package com.example.demo.task;
 
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+import org.springframework.stereotype.Component;
 
 import java.util.concurrent.ThreadPoolExecutor;
 
@@ -18,9 +21,11 @@ import java.util.concurrent.ThreadPoolExecutor;
  * @Date: 2019/11/12 16:48
  * @Version: 1.0
  */
-@Configurable
+//@Configurable
+//    @Component
+@Configuration
 @EnableScheduling
-public class Scheduleconfig implements SchedulingConfigurer {
+public class Scheduleconfig implements SchedulingConfigurer, AsyncConfigurer {
 
 
     @Override
@@ -32,7 +37,9 @@ public class Scheduleconfig implements SchedulingConfigurer {
     public TaskScheduler taskScheduler(){
 
         ThreadPoolTaskScheduler taskScheduler=new ThreadPoolTaskScheduler();
-        taskScheduler.setPoolSize(10); //配置线程进程
+        //初始化话进程
+        taskScheduler.initialize();
+        taskScheduler.setPoolSize(4); //配置线程进程
         taskScheduler.setThreadNamePrefix("spring-task-scheduler-thread-");
         taskScheduler.setAwaitTerminationSeconds(60); //线程池关闭最大等待时间
         taskScheduler.setWaitForTasksToCompleteOnShutdown(true); //线程关闭等待任务完成
--- src/main/java/com/example/demo/task/pojodask.java
+++ src/main/java/com/example/demo/task/pojodask.java
@@ -2,8 +2,6 @@ package com.example.demo.task;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
